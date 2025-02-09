c9aa49d07e06b7fea400d41893ca62813c27cc6d
saba pailodze <159852979+pailodzesaba@users.noreply.github.com>
2025-02-09 21:40:22 +0400

    added movie details functionality

--- src/Components/Atoms/MovieList/MovieList.jsx
+++ src/Components/Atoms/MovieList/MovieList.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, handleSelectMovie }) => {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {movies?.map((movie) => (
-        <li key={movie.imdbID}>
+        <li key={movie.imdbID} onClick={() => handleSelectMovie(movie.imdbID)}>
           <img src={movie.Poster} alt={`${movie.Title} poster`} />
           <h3>{movie.Title}</h3>
           <div>
